b47e45cdd4f8c0a1af9d626aca5ec05792bc848d
snu_osr <usr.snu@gmail.com>
2020-12-04 16:47:59 +0900

    commit for preparing 5th Year Code (Last Year)

---
+++ 3_Code/SNU_v3/src/snu_module/scripts5/run_recognition_module.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Object Recognition Module (SNU Integrated Module)
+for Outdoor Surveillance Robots
+
+    -
+
+"""
+import os, argparse, time, logging, yaml
+import numpy as np
+import rospy
+import tf2_ros
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
